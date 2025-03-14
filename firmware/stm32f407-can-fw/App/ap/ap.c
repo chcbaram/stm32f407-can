082b94097bf20745db39db1a00c63c7cbab85edd
hancheol.cho <chcbaram@gmail.com>
2025-03-14 17:19:19 +0900

    UART 추가

--- firmware/stm32f407-can-fw/App/ap/ap.c
+++ firmware/stm32f407-can-fw/App/ap/ap.c
@@ -19,5 +19,13 @@ void apMain(void)
       pre_time = millis();
       ledToggle(_DEF_LED1);
     }
+
+    if (uartAvailable(_DEF_UART1) > 0)
+    {
+      uint8_t rx_data;
+
+      rx_data = uartRead(_DEF_UART1);
+      uartPrintf(_DEF_UART1, "rx : 0x%02X\n", rx_data);
+    }
   }
 } 
